168191e51c2e0e773bb8fa4d19ffc08ae06d99b7
Karnati Venkat Raja <venkataraja.k@sonata-software.com>
2024-09-30 17:14:40 +0530

    added packages

---
+++ examples/ch02/todo.js
@@ -0,0 +1,54 @@
+const todos = ["coding", "reading newspaper"];
+
+let addTodoInput = document.getElementById("add-todo");
+let addTodoButton = document.getElementById("add-todo-btn");
+let todoList = document.getElementById("todo-list");
+
+for (const todo of todos) {
+  todoList.append(renderTodoInEditMode(todo));
+}
+
+addTodoInput.addEventListener("input", () => {
+  console.log("input entered");
+  addTodoButton.disabled = addTodoInput.value.length < 3;
+});
+
+addTodoInput.addEventListener("keydown", ({ key }) => {
+  if (key === "Enter" && addTodoInput.value.length > 3) {
+    addTodo();
+  }
+});
+
+addTodoButton.addEventListener("click", () => {
+  addTodo();
+});
+
+function renderTodoInReadMode(todo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.textContent = todo;
+  span.addEventListener("dblclick", () => {
+    const idx = todos.indexOf(todo);
+    todosList.replaceChild(
+      renderTodoInEditMode(todo),
+      todosList.childNodes[idx]
+    );
+  });
+  li.append(span);
+  const button = document.createElement("button");
+  button.textContent = "Done";
+  button.addEventListener("click", () => {
+    const idx = todos.indexOf(todo);
+    removeTodo(idx);
+  });
+  li.append(button);
+  return li;
+}
+function removeTodo(index) {
+  // TODO: implement me!
+}
+function addTodo() {
+  ///
+}
+
+function renderTodoInEditMode() {}
